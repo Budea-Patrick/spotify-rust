47822886972223e944cfdffd48e9f4b0ada3f81a
PBUDEA\pbudea <pbudea@montran.com>
2022-09-07 16:56:39 +0300

    authorization creation in web uri

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use urlencoding::encode;
-#[tokio::main]
-async fn main() {
-    let fact = get_cat_fact().await;
+use std::fs::File;
+use std::io::Write;
 
-    println!("fact = {:#?}", fact);
-}
+const CLIENT_ID: &str = "422489842a194b639397736aefc2a55a";
+const _CLIENT_SECRET: &str = "cad6f1f0d0724e50b0ebf9d457142d65";
+const REDIRECT_URI: &str = "http://localhost:63342/spotify-rust/simple_web_request/index.html?_ijt=dr265ocjvqn7enoo2e2lajve0d&_ij_reload=RELOAD_ON_SAVE";
+const AUTHORIZE: &str = "https://accounts.spotify.com/authorize";
 
-async fn get_cat_fact() -> Result<String, Box<dyn std::error::Error>> {
-    let client_id = String::from("422489842a194b639397736aefc2a55a");
-    let _client_secret = String::from("cad6f1f0d0724e50b0ebf9d457142d65");
-    let redirect_uri = String::from("http://localhost:8888/callback");
-    let authorize = String::from("https://accounts.spotify.com/authorize");
-
-    let mut url = authorize.to_string();
+fn create_uri() -> String {
+    let mut url = AUTHORIZE.to_string();
     url.push_str("?client_id=");
-    url.push_str(&client_id);
+    url.push_str(&CLIENT_ID);
     url.push_str("&response_type=code");
     url.push_str("&redirect_uri=");
-    url.push_str(&encode(&redirect_uri));
+    url.push_str(&encode(&REDIRECT_URI));
     url.push_str("&show_dialog=true");
     url.push_str("&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private");
 
+    return url;
+}
+
+async fn get_cat_fact() -> Result<String, Box<dyn std::error::Error>> {
+
+    let mut file = File::create("link.txt").expect("Cannot create file");
+
+    let url = create_uri();
     println!("{}", url);
 
-    let client = reqwest::Client::new();
+    file.write_all(url.as_ref()).expect("Cannot write");
 
+    let client = reqwest::Client::new();
     let body = client.get(url)
         .send()
         .await?
@@ -32,4 +37,11 @@ async fn get_cat_fact() -> Result<String, Box<dyn std::error::Error>> {
         .await?;
 
     Ok(body)
+}
+
+#[tokio::main]
+async fn main() {
+    let fact = get_cat_fact().await;
+
+    println!("fact = {:#?}", fact);
 }
--- target/debug/simple_web_request.d
+++ target/debug/simple_web_request.d
@@ -1,1 +1,1 @@
-D:\Rust\simple_web_request\target\debug\simple_web_request.exe: D:\Rust\simple_web_request\src\main.rs
+C:\Rust\spotify-rust\target\debug\simple_web_request.exe: C:\Rust\spotify-rust\src\main.rs
