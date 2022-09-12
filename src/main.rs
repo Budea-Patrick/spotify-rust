9a450fc7e0c4449a179a0246c32bf30454af233c
PBUDEA\pbudea <pbudea@montran.com>
2022-09-12 11:38:42 +0300

    Getting API response from POST call

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,57 @@
-use std::collections::HashMap;
-
+/// Lightweight RUST based API call to Spotify's API\
+/// Using client credentials flow (see https://developer.spotify.com/documentation/general/guides/authorization/client-credentials/)
+/// API call made using asynchronous runtime using tokio
+/// See Cargo.toml for crate usage
+use crate::header::{HeaderMap, AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE};
 use reqwest::header;
 use reqwest::header::HeaderValue;
-use reqwest::{Body, Client};
-
-use crate::header::{AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE};
+use reqwest::Client;
+use std::collections::HashMap;
 
 const CLIENT_ID: &str = "422489842a194b639397736aefc2a55a";
 const CLIENT_SECRET: &str = "cad6f1f0d0724e50b0ebf9d457142d65";
+const REQUEST_URL: &str = "https://accounts.spotify.com/api/token";
+const POST_TYPE: &str = "application/x-www-form-urlencoded";
 
-async fn get_token() -> Result<String, Box<dyn std::error::Error>> {
+// Spotify requires base64 encoding of client credentials when making API calls
+fn encode_client_credentials() -> String {
     let mut encoded = CLIENT_ID.to_string();
     encoded.push_str(":");
     encoded.push_str(&CLIENT_SECRET);
     encoded = base64::encode(encoded);
+    return encoded;
+}
 
-    let mut header_construct = "Basic ".to_string();
-    header_construct.push_str(&encoded);
+// Generate authorization header
+fn client_authorization(client_credentials: String) -> String {
+    let mut authorization = "Basic ".to_string();
+    authorization.push_str(&client_credentials);
+    return authorization;
+}
 
-    let header_value = header_construct.as_str();
+// Generate request headers
+fn generate_header(authorization_string: String) -> HeaderMap {
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, authorization_string.parse().unwrap());
 
-    println!("{}", header_value);
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert(AUTHORIZATION, header_value.parse().unwrap());
+    // The API call requires the content length to be present
     headers.insert(CONTENT_LENGTH, HeaderValue::from(0));
-    headers.insert(
-        CONTENT_TYPE,
-        HeaderValue::from_static("application/x-www-form-urlencoded"),
-    );
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static(POST_TYPE));
+    return headers;
+}
 
-    let grant_string = "grant_type=client_credentials".to_string();
-    println!("{}", grant_string);
+// Make POST call and return response encapsulated in Box, also checking for errors
+// TODO Replace hardcoded values
+async fn get_response() -> Result<String, Box<dyn std::error::Error>> {
+    let headers = generate_header(client_authorization(encode_client_credentials()));
 
     let mut params = HashMap::new();
     params.insert("grant_type", "client_credentials");
 
     let client = Client::new();
     let body = client
-        .post("https://accounts.spotify.com/api/token")
-        .body(Body::from(grant_string))
+        .post(REQUEST_URL)
+        .query(&params)
         .headers(headers)
         .send()
         .await?
@@ -50,6 +62,6 @@ async fn get_token() -> Result<String, Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() {
-    let fact = get_token().await;
-    println!("fact = {:#?}", fact);
+    let response = get_response().await;
+    println!("response = {:#?}", response);
 }
